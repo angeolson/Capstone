c08ed4c1de28d8510f0aec596973d56e0a5a8fab
angeolson <78054238+angeolson@users.noreply.github.com>
2023-02-03 11:26:50 -0500

    Update data-extract.py
    Completed test case for pulling data for a single subfolder of songs in the test sample

--- data-extract.py
+++ data-extract.py
@@ -10,6 +10,9 @@ import os
 import zipfile
 import json
 import pandas as pd 
+import regex as re 
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
 
 # set environment vars 
 PATH_ = os.getcwd()
@@ -21,44 +24,68 @@ DATAPATH_ = PATH_ + os.sep + 'Data'
 #     zip.extractall(DATAPATH_)
 
 # test
-file = 'TRAAAAW128F429D538.json'
-filepath = DATAPATH_ + '/lastfm_subset/A/A/A/' + file
-with open(filepath) as json_file:
-    data = json.load(json_file)
-    fields = {}
-    fields['artist'] = data['artist']
-    fields['title'] = data['title']
-    fields['tags'] = ", ".join([item[0] for item in data['tags']])
+# file = 'TRAAAAW128F429D538.json'
 
-df = pd.DataFrame(fields, index=[0])
+filepath = DATAPATH_ + '/lastfm_subset/A/A/A/'
+files = os.listdir(filepath)
+df = pd.DataFrame(columns=['artist', 'title', 'tags'])
+for file in files:
+    with open(filepath+file) as json_file:
+        data = json.load(json_file)
+        fields = {}
+        fields['artist'] = data['artist']
+        fields['title'] = data['title']
+        fields['tags'] = ", ".join([item[0] for item in data['tags']])
+        file_df = pd.DataFrame(fields, index=[0])
+        df = pd.concat([df,file_df])
 
-
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
-
-art = df['artist'][0]
-tit = df['title'][0]
+df.reset_index(drop=True)
 
 def cleanTitle(tit):
-    tit = tit.replace("'","")
-    tit = "-".join(tit.split(" "))
+    pattern = re.compile(r'\(.*?\)') # pattern to remove parenthesis, anything in parenthesis 
+    tit = re.sub(pattern, "", tit) # sub pattern
+    tit = re.sub(r'\s+$', '', tit) # remove trailing whitespace
+    tit = tit.replace("'","") # remove apostrephes 
+    tit = "-".join(tit.split(" ")) # join each word with a dash
     return tit
 
-artist = "-".join(art.split(" "))
-song = cleanTitle(tit)
 
-URL = f'https://genius.com/{artist}-{song}-lyrics'
-req = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
-web_byte = urlopen(req).read()
-soup = BeautifulSoup(web_byte, "html.parser")
-# page = requests.get(URL)
-# html = BeautifulSoup(page.text, "html.parser") # Extract the page's HTML as a string
+df['artist_lookup'] = df['artist'].apply(lambda x:"-".join(x.split(" ")))
+df['title_lookup'] = df['title'].apply(cleanTitle)
+# artist = "-".join(art.split(" "))
+# song = cleanTitle(tit)
 
-# Scrape the song lyrics from the HTML
-lyrics = soup.find("div", class_="Lyrics__Container-sc-1ynbvzw-6 YYrds").get_text()
+def getLyrics(artist,song):
+    try:
+        URL = f'https://genius.com/{artist}-{song}-lyrics'
+        req = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+        web_byte = urlopen(req).read()
+        soup = BeautifulSoup(web_byte, "html.parser")
+    except: return "song not found"
+    # Scrape the song lyrics from the HTML
+    try:
+        lyrics = soup.find("div", class_="Lyrics__Container-sc-1ynbvzw-6 YYrds").get_text()
+    except: return "lyrics not found"
+    return lyrics 
 
-import regex as re 
-results = re.findall(r'[a-z]+[A-Z][a-z]+', lyrics) # finds all occurences of words that should be split up 
-pattern = re.compile(r'[a-z]+[A-Z][a-z]+')
+def getVerseTypes(lyrics):
+    verse_types = re.findall(r'\[.*?\]', lyrics)
+    return verse_types
 
-" ".join(re.sub( r"([A-Z])", r" \1", lyrics).split()) # works 
+def getVerses(lyrics):
+    pattern = re.compile(r'\[.*?\]')
+    split_lyrics = [item for item in re.split(pattern, lyrics) if len(item) > 0]
+    split_lyrics_delimited = [re.sub(r'(?<![A-Z\W])(?=[A-Z])', '-', item) for item in split_lyrics]
+    split_lyrics_lines = [verse.split("-") for verse in split_lyrics_delimited]
+    split_lyrics_lines_clean = [[item for item in verse if len(item) > 0] for verse in split_lyrics_lines]
+    return split_lyrics_lines_clean
+
+def dataframeLyrics(df):
+    artist = df['artist_lookup']
+    song = df['title_lookup']
+    lyrics = getLyrics(artist, song)
+    return lyrics 
+
+df['lyrics'] = df.apply(dataframeLyrics, axis=1)
+df['verses'] = df['lyrics'].apply(getVerses)
+df['verse_types'] = df['lyrics'].apply(getVerseTypes)
