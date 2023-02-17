deb8e5eedf791b427a1de1bdb07cf307a890ffa6
angeolson <78054238+angeolson@users.noreply.github.com>
2023-02-17 12:22:17 -0500

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -1,19 +1,9 @@
 # imports
 import pandas as pd
 import regex as re
-
-# init dict of verse types to keep
-typedict = {'verse': 1,
-            'chorus': 2,
-            'pre chorus': 3,
-            'bridge': 4,
-            'outro': 5,
-            'intro': 6,
-            'refrain': 7,
-            'hook': 8,
-            'instrumental': 9,
-            'post chorus': 10
-}
+import spacy
+from spacy.language import Language
+from spacy_langdetect import LanguageDetector
 
 # helper functions
 def cleanData(df):
@@ -29,6 +19,12 @@ def cleanData(df):
     return df
 
 def removeX(item):
+    '''
+    function removes 'x' from verse type
+
+    :param item: string
+    :return: item without 'x'
+    '''
     if item is None:
         return item
     elif item.lower() == 'x':
@@ -36,6 +32,13 @@ def removeX(item):
     else:
         return item
 def concatType(item, type='chorus'):
+    '''
+    function removes words that come after the specified verse type to get it down to its 'root type'
+
+    :param item: string
+    :param type: string
+    :return: item where item == type or None
+    '''
     if item is None:
         return item
     elif item.startswith(type):
@@ -44,12 +47,26 @@ def concatType(item, type='chorus'):
         return item
 
 def keepTypes(item):
+    '''
+    function limits the different type names kept in the dataframe
+
+    :param item: string
+    :return: item in list typedict.keys()
+    '''
     if item in typedict.keys():
         return item
     else:
-        return ''
+        return 'other'
 
 def verseTypeCleaner(verse_type):
+    '''
+    this function splits any verse type at the ':' delimeter and keeps the first hald, removes numbers and select punctuation,
+    removes trailing/leading whitespace, lowercases words, gets verse types down to their root (e.g. "chorus" instead of "chorus repeated"),
+    and removes the 'x' label. Lastly, this function removes types that are uncommon and replaces them with blank space.
+
+    :param verse_type:
+    :return: cleaned verse type list
+    '''
     new_types = [item.split(':')[0] for item in verse_type] # only keep verse title before [:]
     new_types = [re.sub(r'[\d\[\].!?\\]', '', item) for item in new_types] # remove numbers, punctuation exept -
     new_types = [re.sub(r'\-', ' ', item) for item in new_types]
@@ -61,28 +78,90 @@ def verseTypeCleaner(verse_type):
     new_types = [keepTypes(item) for item in new_types]
     return new_types
 
+def get_lang_detector(nlp, name):
+    return LanguageDetector()
+
+def isEnglish(song):
+    '''
+    determines if a song is englihs using spacy english model
+
+    :param song: verse column of dataframe
+    :return: boolean if the song is english or not
+    '''
+    sentence_list = [sentence for verse in song for sentence in verse]
+    text = " ".join(sentence_list)
+    doc = nlp(text)
+    detect_language = doc._.language
+    if detect_language['language'] == 'en':
+        return True
+    else:
+        return False
+
+def VerseCleaner(df):
+    '''
+    cleans the verses of the dataframe; removes songs that are not english
+
+    :param df: pandas dataframe
+    :return: df
+    '''
+    df['english'] = df['verses'].apply(isEnglish)
+    df = df[ df['english'] is True]
+    return df
+
+# init dict of verse types to keep
+typedict = {'verse': '<VERSE>',
+            'chorus': '<CHORUS>',
+            'pre chorus': '<PRECHORUS>',
+            'bridge': '<BRIDGE>',
+            'outro': '<OUTRO>',
+            'intro': '<INTRO>',
+            'refrain': '<REFRAIN>',
+            'hook': '<HOOK>',
+            'instrumental': '<INSTRUMENTAL>',
+            'post chorus': '<POSTCHORUS>',
+            'other': '<OTHER>'
+}
+
+# init spacy
+nlp = spacy.load("en_core_web_sm")
+Language.factory("language_detector", func=get_lang_detector)
+nlp.add_pipe('language_detector', last=True)
+
 # read in data
 df = pd.read_csv('data_delineated.csv')
 
 # cleaning pipeline for dataframe
 df = cleanData(df)
-
 # step one: what are the types of verses in the songs?
-print(df.verse_types.explode().dropna().value_counts())
+# print(df.verse_types.explode().dropna().value_counts())
 
-# a lot of verse names seem to have extra information after the colon, or the '[' and ']' is missing and causing duplicates. We also probably don't need verse numbers.
+# cleaning pipeline for verse types
 df['verse_types'] = df['verse_types'].apply(verseTypeCleaner)
-
-print(df.verse_types.explode().dropna().value_counts())
-
+# print(df.verse_types.explode().dropna().value_counts())
 # observe as .csv file
 # df.verse_types.explode().dropna().value_counts().reset_index().to_csv('verse_types.csv')
 
+# cleaning pipeline for verses
+# to do:
+# 1: insert spaces between punctuation marks and letters
+# 2: determine if the song is in english: if it is, keep it. Otherwise, delete song from dataframe
+# 3: create a new column for each song, which will be the verses in the format we want them: add in special tokens for verse type, line breaks, but otherwise unpack lists
 
-# To do:
-# replace x with blank
-# want anything that's not chorus, verse, prechorus, bridge, outro, intro, refrain, hook to be blank
-# turn Chorus x into Chorus
-# want Chorus I = Chorus II = Chorus
-# want verse = VERSE = Verse
-# remove non-english songs from frame
+# os.system('pip3 install markupsafe==2.0.1') # last compatible version of markupsafe to use with language detector
+
+
+test = df.iloc[0]
+len(test.verses)
+len(test.verse_types)
+
+for i in range(len(test.verses)):
+    list_ = []
+    list_.append(typedict[test.verse_types[i]])
+    sentence_list = [sentence for sentence in test.verses[i]]
+    for item in sentence_list:
+        list_.append('<NEWLINE>')
+        for token in item.split(" "):
+            list_.append(token)
+    print(list_)
+
+
