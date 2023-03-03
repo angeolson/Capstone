85d9dc9975ce8d3d3441dc3d74286476034917f8
angeolson <78054238+angeolson@users.noreply.github.com>
2023-03-03 15:29:44 -0500

    update lstm-model1.py

--- lstm-model1.py
+++ lstm-model1.py
@@ -1,4 +1,4 @@
-'''Initial Model based on tutorial from https://closeheat.com/blog/pytorch-lstm-text-generation-tutorial'''
+# Initial Model based on tutorial from https://closeheat.com/blog/pytorch-lstm-text-generation-tutorial
 
 # imports
 import pandas as pd
@@ -11,8 +11,9 @@ import random
 
 SEED = 48
 random.seed(48)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-#-----------HELPER FUNCTIONS------------
+# -----------HELPER FUNCTIONS------------
 def cleanData(df):
     '''
     function cleans the original pandas dataframe imported from .csv and returns a clean frame
@@ -23,46 +24,109 @@ def cleanData(df):
         df[col] = df[col].apply(lambda x:eval(x, {'__builtins__': None}, {}))
     return df
 
+def tokenizer(x):
+    return x.split(" ")
+
 
 #---------SET VARS--------------------
-EPOCHS = 5
-BATCH_SIZE = 256
-SEQUENCE_LEN = 50
+EPOCHS = 2
+BATCH_SIZE = 512
+SEQUENCE_LEN = 4
+MAX_LEN = 210
 #--------CLASS DEFINITIONS-------------
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(
         self,
-        sequence_length
+        dataframe,
+        sequence_length,
+        tokenizer,
+        batch_size,
+        max_len
     ):
-        self.words = self.load_words()
+        self.dataframe = dataframe
+        self.words = self.load_words(self.dataframe)
         self.uniq_words = self.get_uniq_words()
         self.index_to_word = {index: word for index, word in enumerate(self.uniq_words)}
         self.word_to_index = {word: index for index, word in enumerate(self.uniq_words)}
         self.words_indexes = [self.word_to_index[w] for w in self.words]
         self.sequence_length = sequence_length
-    def load_words(self):
-        df = pd.read_csv('df_LSTM.csv', index_col=0)
-        df.reset_index(drop=True, inplace=True)
-        text = df['lyrics'].str.cat(sep=' ')
+        self.tokenizer = tokenizer
+        self.batch_size = batch_size
+        self.max_len = max_len
+
+    def load_words(self, dataframe):
+        text = dataframe['lyrics'].str.cat(sep=' ')
         return text.split(' ')
     def get_uniq_words(self):
         word_counts = Counter(self.words)
-        return sorted(word_counts, key=word_counts.get, reverse=True)
-    def __len__(self):
-        return len(self.words_indexes) - self.sequence_length
-    def __getitem__(self, index):
-        return (
-            torch.tensor(self.words_indexes[index:index+self.sequence_length]),
-            torch.tensor(self.words_indexes[index+1:index+self.sequence_length+1]),
-        )
+        unique_words = sorted(word_counts, key=word_counts.get, reverse=True)
+        unique_words_padding = ['PAD'] + unique_words
+        return unique_words_padding
 
-import torch
-from torch import nn
+    def __len__(self):
+        return len(self.dataframe)
+
+    def build_sequences(self, text, word_to_index, sequence_length, max_len):
+        '''
+        adapted from https://gist.github.com/FernandoLpz/acaeb5fe714d084c0fe08481fdaa08b7#file-build_sequences-py
+        :param text:
+        :param word_to_index:
+        :param sequence_length:
+        :return:
+        '''
+        x = list()
+        y = list()
+
+        for i in range(max_len):
+            try:
+                # Get window of chars from text
+                # Then, transform it into its idx representation
+                sequence = text[i:i + sequence_length]
+                sequence = [word_to_index[char] for char in sequence]
+
+                # Get word target
+                # Then, transfrom it into its idx representation
+                target = text[i + sequence_length]
+                target = word_to_index[target]
+
+                # Save sequences and targets
+                x.append(sequence)
+                y.append(target)
+
+            except:
+                sequence = [0]*sequence_length
+                x.append(sequence)
+                y.append(0)
+
+        x = np.array(x)
+        y = np.array(y)
+
+        return x, y
+
+    # def __data_generation(self, sequences_batch, next_words_batch):
+    #     X = np.zeros((self.batch_size, self.sequence_length, len(self.uniq_words)), dtype=np.bool)
+    #     y = np.zeros((self.batch_size, len(self.uniq_words)), dtype=np.bool)
+    #
+    #     for i, seq in enumerate(sequences_batch):
+    #         for j, word in enumerate(seq):
+    #             X[i, j, word] = 1
+    #             y[i, next_words_batch[i]] = 1
+    #     return X, y
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        input = self.dataframe.iloc[idx]['lyrics']
+        tokenized_input = self.tokenizer(input)
+        X, y = self.build_sequences(text=tokenized_input, word_to_index=self.word_to_index, sequence_length=self.sequence_length, max_len=self.max_len)
+
+        return torch.tensor(X), torch.tensor(y)
+
 class Model(nn.Module):
     def __init__(self, dataset):
         super(Model, self).__init__()
-        self.lstm_size = 128
+        self.hidden_dim = 128
         self.embedding_dim = 128
         self.num_layers = 3
         n_vocab = len(dataset.uniq_words)
@@ -71,20 +135,38 @@ class Model(nn.Module):
             embedding_dim=self.embedding_dim,
         )
         self.lstm = nn.LSTM(
-            input_size=self.lstm_size,
-            hidden_size=self.lstm_size,
+            input_size=self.embedding_dim,
+            hidden_size=self.hidden_dim,
             num_layers=self.num_layers,
             dropout=0.2,
+            batch_fist=True
         )
         self.fc = nn.Linear(self.lstm_size, n_vocab)
-    def forward(self, x, prev_state):
+
+    def forward(self, x, hidden):
         embed = self.embedding(x)
-        output, state = self.lstm(embed, prev_state)
-        logits = self.fc(output)
-        return logits, state
+        output, hidden = self.lstm(embed, hidden)
+        out = self.fc(output)
+        return out, hidden
+    def forward(self, x, hidden, attention_mask):
+        batch_size = x.size(0)
+        embedded = self.bert(input_ids=x,attention_mask=attention_mask)[0]
+        #input = embedded.permute(1, 0, 2)
+        lstm_out, hidden = self.lstm(embedded, hidden)
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        out = out.view(batch_size, -1, self.output_dim)
+        out = out[:, -1]
+        return out, hidden
     def init_state(self, sequence_length):
         return (torch.zeros(self.num_layers, sequence_length, self.lstm_size),
                 torch.zeros(self.num_layers, sequence_length, self.lstm_size))
+
+    def init_hidden(self, batch_size, ):
+        h0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
+        c0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
+        return h0, c0
 
 #--------------MODEL FUNCTIONS----------------
 def train(dataset, model, batch_size, sequence_length, max_epochs):
@@ -94,8 +176,11 @@ def train(dataset, model, batch_size, sequence_length, max_epochs):
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     for epoch in range(max_epochs):
         state_h, state_c = model.init_state(sequence_length)
-        for batch, (x, y) in enumerate(dataloader):
+        #for batch, (x, y) in enumerate(dataloader):
+        for batch in dataloader:
             optimizer.zero_grad()
+            x = batch[0]
+            y = batch[1]
             y_pred, (state_h, state_c) = model(x, (state_h, state_c))
             loss = criterion(y_pred.transpose(1, 2), y)
             state_h = state_h.detach()
@@ -118,8 +203,16 @@ def predict(dataset, model, text, next_words=100):
     return words
 
 #-------------MODEL PREP----------------
-dataset = Dataset(sequence_length=SEQUENCE_LEN)
+df = pd.read_csv('df_LSTM.csv', index_col=0)
+df.reset_index(drop=True, inplace=True)
+dataset = Dataset(dataframe=df, sequence_length=SEQUENCE_LEN, tokenizer=tokenizer, batch_size=BATCH_SIZE, max_len=MAX_LEN)
 model = Model(dataset)
+
+
+# testing
+# dataloader = DataLoader(dataset, batch_size=BATCH_SIZE)
+# for batch in dataloader:
+#     break
 
 #------------MODEL TRAIN----------------
 train(dataset, model, batch_size=BATCH_SIZE, sequence_length=SEQUENCE_LEN, max_epochs=EPOCHS)
