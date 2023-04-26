1225df0980e9ab74d283a0ac4fc969e67412f43a
angeolson <78054238+angeolson@users.noreply.github.com>
2023-04-26 16:48:07 -0400

    Update lstm-model4.py

--- Final-Model/lstm-model4.py
+++ Final-Model/lstm-model4.py
@@ -9,6 +9,7 @@ from transformers import BertTokenizer, BertModel, BertConfig
 from dataset import Dataset
 from model import Model
 import os
+import argparse
 
 SEED = 48
 random.seed(48)
@@ -16,19 +17,39 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 # ---------SET VARS--------------------
-EPOCHS = 50
-MAX_LEN = 250
-SEQUENCE_LEN = 4
-LR = 0.001
-TRUNCATE = False
-DF_TRUNCATE_LB = 0  # lower bound to truncate data
-DF_TRUNCATE_UB = 1000  # upper bound to truncate data
-Iterative_Train = False  # False if training model from scratch, True if fine-tuning
-single_token_output = False  # True if only want to look at last word logits
-load_model = 'model-4-all.pt'
-save_model = 'model-4-hs128-2fc-vocab_trunc.pt'
-filepath_for_losses = 'epoch_losses_m4_hs128-2fc-vocab_trunc.csv'
+parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter)
+parser.add_argument("-dt_pth", "--data_path", default = 'None', type=str, help = "path to the data files", required=True)
+parser.add_argument("-ex_pth", "--export_path", default = 'None', type=str, help = "path for exported files", required=True)
+parser.add_argument("-e", "--epochs", default = 50, type=int, help = "number of epochs to run training for", required=True)
+parser.add_argument("-mlen", "--max_len", default = 250, type=int, help = "max length to truncate songs", required=True)
+parser.add_argument("-slen", "--seq_len", default = 250, type=int, help = "sequence length for inputs and outputs", required=True)
+parser.add_argument("-lr", "--learning_rate", default = 0.001, type=int, help = "learning rate", required=True)
+parser.add_argument("-trunc", "--truncate", default = False, type=bool, help = "if you would like to truncate the dataset", required=True)
+parser.add_argument("-lb", "--lower_bound", default = 0, type=int, help = "truncation lower bound", required=False)
+parser.add_argument("-ub", "--upper_bound", default = 0, type=int, help = "truncation upper bound", required=False)
+parser.add_argument("-iter", "--iterative_train", default = False, type=bool, help = "True if fine-tuning or training off stopping point, False if training from scratch", required=True)
+parser.add_argument("-sing", "--single_token", default = False, type=bool, help = "True if only want to look at last word logits", required=True)
+parser.add_argument("-load", "--load_model", default = 'None', type=str, help = "If fine-tuning or training off stopping point, load previous model", required=False)
+parser.add_argument("-save", "--save_model", default = 'model-4-hs128-2fc-vocab_trunc.pt', type=str, help = "name of model to save", required=True)
+parser.add_argument("-file", "--loss_file", default = 'epoch_losses_m4_hs128-2fc-vocab_trunc.csv', type=str, help = "name of file to save epoch loss data to", required=True)
+parser.add_argument("-v", "--verbose", default = False, type=bool, help = "True if want to see loss for each batch for each epoch", required=True)
+args = vars(parser.parse_args())
 
+EPOCHS = args['epochs']
+MAX_LEN = args['max_len']
+SEQUENCE_LEN = args['seq_len']
+LR =  args['learning_rate']
+TRUNCATE = args['truncate']
+DF_TRUNCATE_LB = args['lower_bound']
+DF_TRUNCATE_UB = args['upper_bound']
+Iterative_Train = args['iterative_train']
+single_token_output = args['single_token']
+load_model = args['load_model']
+save_model = args['save_model']
+filepath_for_losses = args['file']
+DATA_PATH = args['data_path']
+EXPORT_PATH = args['export_path']
+VERBOSE = args['verbose']
 
 # -----------HELPER FUNCTIONS------------
 def cleanData(df):
@@ -76,7 +97,8 @@ def train(train_dataset, val_dataset, model, max_epochs, lr):
                 state_c = state_c.detach()
                 loss.backward()
                 train_losses.append(loss.item())
-                print({'epoch': epoch, 'batch': train_batch_count, 'train loss': loss.item()})
+                if VERBOSE is True:
+                    print({'epoch': epoch, 'batch': train_batch_count, 'train loss': loss.item()})
                 train_batch_count += 1
             optimizer.step()
         model.eval()
@@ -93,7 +115,8 @@ def train(train_dataset, val_dataset, model, max_epochs, lr):
                 val_state_h = val_state_h.detach()
                 val_state_c = val_state_c.detach()
                 val_losses.append(val_loss.item())
-                print({'epoch': epoch, 'batch': val_batch_count, 'val loss': val_loss.item()})
+                if VERBOSE is True:
+                    print({'epoch': epoch, 'batch': val_batch_count, 'val loss': val_loss.item()})
                 val_batch_count += 1
 
         epoch_train_loss = np.mean(train_losses)
@@ -113,7 +136,7 @@ def train(train_dataset, val_dataset, model, max_epochs, lr):
 
 if __name__ == '__main__':
     # ---------LOAD DATA--------------------
-    os.chdir('/home/ubuntu/Capstone')
+    os.chdir(DATA_PATH)
     df_train = pd.read_csv('training.csv', index_col=0)
     df_train.reset_index(drop=True, inplace=True)
 
@@ -156,6 +179,6 @@ if __name__ == '__main__':
         model.load_state_dict(torch.load(load_model, map_location=device))
 
     # ------------MODEL TRAIN----------------
-    os.chdir('/home/ubuntu/Capstone/Final-Model')
+    os.chdir(EXPORT_PATH)
     # train(train_dataset=train_dataset, val_dataset=val_dataset, model=model, batch_size=BATCH_SIZE, max_epochs=EPOCHS, seq_len=SEQUENCE_LEN)
     train(train_dataset=train_dataset, val_dataset=val_dataset, model=model, max_epochs=EPOCHS, lr=LR)
